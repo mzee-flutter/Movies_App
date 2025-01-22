cc0a315800c44556351af5148e91a7f54ca9f178
mzee-flutter <mudair.khan37@gmail.com>
2025-01-22 23:49:13 +0400

    this is the file that contain color that frequently use in this app

---
+++ lib/utilities/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color appColor = Color(0xff202123);
+
+const Color whiteColor = Colors.white;
+const Color blackColor = Colors.black;
+const Color redColor = Color(0xffF60002);
