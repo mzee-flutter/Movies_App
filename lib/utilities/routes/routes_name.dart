b3c0455603777b3344b43a9bc49ce0125d1c9659
mzee-flutter <mudair.khan37@gmail.com>
2025-01-23 23:35:25 +0400

    these are the routes names by which we do routing

---
+++ lib/utilities/routes/routes_name.dart
@@ -0,0 +1,9 @@
+class RoutesName {
+  static const String loginScreen = 'login-view';
+  static const String homeScreen = 'home_screen';
+  static const String signupScreen = 'signup_screen';
+  static const String splashScreen = 'splash_screen';
+  static const String moviesCategoryScreen = 'movies_category_screen';
+  static const String tvShowsCategoryScreen = 'tvShows_category_screen';
+  static const String fullMovieInfoScreen = 'full_movie_info_screen';
+}
